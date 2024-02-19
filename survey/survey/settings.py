a758383c5772629a3d0d6a096000caeb52ec8a7b
--replace-all <stefan.gavriliuc@ucalgary.ca>
2024-02-18 21:03:00 -0700

    chore: db + reqs config

--- survey/survey/settings.py
+++ survey/survey/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'survey.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'survey',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
